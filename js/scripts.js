9ce6af64aec3fc44b8b5e9e99decd90e15286bae
Maggie Summers and Stephanie Faber <maggie@>
2018-01-17 13:11:57 -0800

    initialize base files for project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var numbers = [];
+
+function elim(prime) {
+  for (j = 2; j <= n; j++) {
+    return prime * j;
+  }
+}
+
+//creates array
+for (i = 2; i <= n; i++) {
+  numbers.push(i);
+}
+
+//cycles array
+for (var prime = 2; prime <= n; prime++) {
+  delete numbers(elim(prime));
+}
