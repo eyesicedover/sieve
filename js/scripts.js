dfc6acd1e2cbc4842590181e76ea7fc187b35d5c
Maggie Summers and Stephanie Faber <stephanie@>
2018-01-17 15:36:34 -0800

    did the whole darn thing

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,43 @@
 var numbers = [];
+var nonprimeArray= [];
 
-function elim(prime) {
-  for (j = 2; j <= n; j++) {
-    return prime * j;
-  }
-}
+$(document).ready(function() {
+  $("form#numbersWhatever").submit(function(event) {
+    event.preventDefault();
+    var n = $("input#userNum").val();
 
-//creates array
-for (i = 2; i <= n; i++) {
-  numbers.push(i);
-}
+    //creates array
+    for (i = 2; i <= n; i++) {
+      numbers.push(i);
+    }
 
-//cycles array
-for (var prime = 2; prime <= n; prime++) {
-  delete numbers(elim(prime));
-}
+    function nonprime() {
+    	for (var j = 2; j <= 9; j++) {
+      	for (var k = 2; k<= n; k++) {
+       	nonprimeArray.push(j * k);
+        }
+    	}
+    }
+    nonprime();
+
+    for (l = 0; l <= n; l++) {
+    	for (d = 0; d <= nonprimeArray.length; d++)
+    	if (numbers[l] === nonprimeArray[d]) {
+      delete numbers[l];
+      }
+    }
+
+    Array.prototype.clean = function(deleteValue) {
+      for (var i = 0; i < this.length; i++) {
+        if (this[i] == deleteValue) {
+          this.splice(i, 1);
+          i--;
+        }
+      }
+      return this;
+    };
+
+    $("#output").text(numbers.clean(undefined));
+    });
+
+});
